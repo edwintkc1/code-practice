b1bdef2c6f60bcf9cfb3e653a32431bb3d360cff
edwintkc <edwintkc1@gmail.com>
2020-11-24 00:07:54 +0800

    add 755

---
+++ 11-2020/755 Stock market (~feel good)/brute force.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#define ll long long
+using namespace std;
+
+//brute froce, 先定點，左右開始掃過去x (step) 步
+//每次睇下邊個最細，用min*step，++step
+//直到果個點試曬所有step乘min得嘅tempMax
+//換下一個點，重複以上步驟
+//使曬所有點=試曬所有可能性，得出max
+
+int main()
+{
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int N, temp;
+		cin >> N;
+		vector<ll> stocks;
+		for (int i = 0; i < N; i++) // input
+		{
+			cin >> temp;
+			stocks.push_back(temp);
+		}
+
+		ll max = 0;
+		for (int i = 0; i < N; i++)
+		{
+			ll minValue = INT64_MAX;
+			for (int step = 0; step <= N; step++)
+			{
+				for (int j = i; j < step && j <= N; j++)
+					minValue = min(minValue, stocks[j]);
+				for (int j = i; j > i - step && j >= 0; j--)
+					minValue = min(minValue, stocks[j]);
+
+				ll tempMax = minValue * step;
+				if (tempMax > max)
+					max = tempMax;
+			}
+		}
+		cout << max << endl;
+
+	}
+
+	return 0;
+}
---
+++ 11-2020/755 Stock market (~feel good)/monotonous stack.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#define ll long long
+using namespace std;
+
+// 單調遞增棧 montontone stack
+// https://www.youtube.com/watch?v=GYuBQacXr1A 
+// leetcode 84
+
+int main()
+{
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int N, temp;
+		cin >> N;
+		vector<ll> stocks;
+		for (int i = 0; i < N; i++) // input
+		{
+			cin >> temp;
+			stocks.push_back(temp);
+		}
+
+		stack<ll> stack;
+		stack.push(-1);
+		ll maxValue = 0;
+		for (int i = 0; i < N; i++)
+		{
+			while (stack.top() != -1 && stocks[stack.top()] >= stocks[i])
+			{
+				ll temp = stocks[stack.top()];
+				stack.pop();
+				temp *= (i - stack.top() - 1);
+				maxValue = max(maxValue, temp);
+			}
+
+			stack.push(i);
+
+		}
+		while (stack.top() != -1)
+		{
+			ll temp = stocks[stack.top()];
+			stack.pop();
+			temp *= (stocks.size() - stack.top() - 1);
+			maxValue = max(maxValue, temp);
+		}
+		cout << maxValue << endl;
+
+	}
+
+	return 0;
+}
