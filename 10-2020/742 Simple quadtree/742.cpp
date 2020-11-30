9c52d06d4f3d31c2c033287d430122860221c9c1
edwintkc <edwintkc1@gmail.com>
2020-12-01 00:13:00 +0800

    rectify 742

--- 10-2020/742 Simple quadtree/742.cpp
+++ 10-2020/742 Simple quadtree/742.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
+
+//每次將area分做4份，recursively 傳入去再分
+//直到最後分到2x2，就送入去check下一唔一樣
+//一樣就set做等於1，唔一樣就加曬佢
 
 bool compare(vector<string>& graph, int size, int x, int y)
 {
@@ -11,58 +15,55 @@ bool compare(vector<string>& graph, int size, int x, int y)
 	return false;
 }
 
-int count(vector<string>& graph, int size, int x, int y)
-{
+int count(vector<string> &graph, int x, int y, int size) {
+	if (size == 1)
+		return 1;
 
 	int newSize = size / 2;
-	if (newSize == 1) // now the graph is 2x2, send in compare 4 boxes
+	if (newSize == 1) // if newSize = 1, oldSize = 2 = the graph now is 2x2, send in compare 4 boxes
 	{
 		if (compare(graph, newSize, x, y) == true)
 			return 1;
-		return 5;
+		else
+			return 5;
 	}
 	else // keep dividing the graph into 4 small pieces
 	{
 		int sum = 0;
-		int topLeft = count(graph, newSize, x, y);
-		int topRight = count(graph, newSize, x + newSize, y);
-		int bottomLeft = count(graph, newSize, x, y + newSize);
-		int bottomRight = count(graph, newSize, x + newSize, y + newSize);
 
-		if (((topLeft == topRight == bottomLeft == bottomRight) == 1))
-			sum = 1;
-		else
+		int topLeft = count(graph, x, y, newSize);
+		int topRight = count(graph, x + newSize, y, newSize);
+		int bottomLeft = count(graph, x, y + newSize, newSize);
+		int bottomRight = count(graph, x + newSize, y + newSize, newSize);
+
+		if (topLeft == 1 && topRight == 1 && bottomLeft == 1 && bottomRight == 1 && compare(graph, newSize, x, y))
+			sum = 1; // all 4 areas are same
+		else // not same
 			sum = topLeft + topRight + bottomLeft + bottomRight;
 
-		if (sum != 1) // if all same no need to + 1
-			sum++; // root 
+		if (sum != 1) // if all 4 areas are not same
+			sum += 1; // add the root
+
 		return sum;
 	}
 }
 
-
-int main()
-{
+int main() {
 	int k;
-	while (cin >> k)
-	{
+	while (cin >> k) {
 		int n = 1;
-		n = n << k;  // power
+		n = n << k; // n = 2^k
 
 		vector<string> graph;
-		graph.resize(n);
-
 		string line;
 		int graphIndex = 0;
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < n; i++) // input
 		{
 			cin >> line;
-			graph[graphIndex++] = line;
+			graph.push_back(line);
 		}
 
-		int ret = count(graph, n, 0, 0);
-		cout << ret << endl;
+		cout << count(graph, 0, 0, n) << endl;
 	}
-
 	return 0;
 }
